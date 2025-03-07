0ded0cdd043b40abc35f7be0110267cfdc3578ce
Jack Schwendeman <jack.schwendeman@gmail.com>
2025-03-06 20:06:42 -0600

    Mockup DDL script

---
+++ sql/DDL scripts/create_tables.sql
@@ -0,0 +1,74 @@
+CREATE TABLE Account (
+	id			int PRIMARY KEY AUTO_INCREMENT,
+	email		varchar(50) UNIQUE NOT NULL,
+    password	varchar(50) NOT NULL,
+    name		varchar(50) NOT NULL
+);
+
+-- option user details to configure --
+CREATE TABLE UserDetails (
+	userId		int PRIMARY KEY,
+	dob			date,
+    firstName	varchar(50),
+    lastName	varchar(50),
+    height		float,
+    
+    -- could be compared against reports --
+    startWeight		float,
+    
+    -- misc descriptive fields about user conditions that could be shown when viewing account --
+    allergies		varchar(200),
+    conditions		varchar(500),
+    medicalHistory  varchar(1000),
+    
+    FOREIGN KEY (userId) REFERENCES Account(id)
+);
+
+CREATE TABLE Reminder (
+	userId			int PRIMARY KEY,
+    description		varchar(100),
+    dueDate			date,
+    FOREIGN KEY (userId) REFERENCES Account(id)
+);
+
+-- user goals to configure --
+CREATE TABLE UserGoals (
+	dailyExercise			time,
+    dailyCaloriesConsumed	float,
+    dailyCaloriesBurned		float,
+    desiredWeight			float
+);
+
+CREATE TABLE WeightReport (
+	userId	int PRIMARY KEY,
+	weight	float NOT NULL,
+    logDate	date NOT NULL,
+    FOREIGN KEY (userId) REFERENCES Account(id)
+);
+
+CREATE TABLE ExerciseReport (
+	userId 			int PRIMARY KEY,
+    category		ENUM ('Aerobic', 'Strength', 'Stretching', 'Other')  NOT NULL,
+    duration		time NOT NULL,
+    burnedCalories  float NOT NULL,
+    logDate			date NOT NULL,
+	FOREIGN KEY (userId) REFERENCES Account(id)
+);
+
+CREATE TABLE MealReport (
+	userId			int PRIMARY KEY,
+    category		ENUM ('Fruit', 'Vegetable', 'Grain', 'Protein', 'Dairy', 'Fat', 'Snack', 'Sweet', 'Beverage') NOT NULL,
+    description		varchar(25) NOT NULL,
+    calories		float NOT NULL,
+    logDate			date NOT NULL,
+    FOREIGN KEY (userId) REFERENCES Account(id)
+);
+
+CREATE TABLE MentalHealthReport (
+	userId				int PRIMARY KEY,
+    -- some score determined by the response in the applet (ex add up question answers ranked on scale 1-10) --
+    anxietyScore		int NOT NULL,
+    depressionScore		int NOT NULL,
+    logDate				date NOT NULL,
+    FOREIGN KEY (userID) REFERENCES Account(id)
+)
